d04899e5508a46fd435fcc8c391ac284eb5bc320
toashel <shelchean@gmail.com>
2017-02-01 12:02:55 +1300

    Add reddit url and event listener

--- js/program.js
+++ js/program.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
-	
+
 	chrome.browserAction.setBadgeText({
 		text: ""
 	});
-	
+
 	var apiUrl = 'https://api.tumblr.com/v2/blog/lovelivescans/posts/photo/?api_key=iOWuHVlzVyFGjvKGHSB1zro7RRgQbAwsGuW5VJhMwtYACWBg78&limit=1';
+	var redditUrl = 'https://www.reddit.com/r/manga/search.json';
 
 	var opt = {
 		type: "basic",
@@ -13,7 +14,7 @@ $(document).ready(function() {
 		iconUrl: "img/icon.png"
 	};
 
-	var notification = chrome.notifications.create(opt);
+	// var notification = chrome.notifications.create(opt);
 
 	$.get(apiUrl, function() {})
 		.done(function(data) {
@@ -31,11 +32,33 @@ $(document).ready(function() {
 					url: $(this).attr('href')
 				});
 			});
+
+			$.ajax({
+				type: 'GET',
+				url: redditUrl,
+				data: {
+					q: '[DISC] Tomo-chan wa Onnanoko! ' + chapterNum
+				}
+				})
+				.done(function(redditData) {
+					// console.log(redditData);
+					var redditPost = redditData.data.children[0];
+					$('#latest-reddit').attr('href', 'https://reddit.com' + redditPost.data.permalink);
+					$('#latest-reddit').removeClass('disabled');
+
+					$('#latest-reddit').click(function() {
+						chrome.tabs.create({
+							url: $(this).attr('href')
+						});
+					});
+
+				});
 		})
 		.fail(function(err) {
 			console.log(err);
 			$('#ajax-err').parent().show();
 			document.getElementById('ajax-err').innerHTML = err;
 		});
+
 
 });
